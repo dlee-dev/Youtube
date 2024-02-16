308451d855c266bac532f491a9f7396a5812e53a
dlee-dev <david.y.lee.dev@gmail.com>
2024-02-16 13:47:39 -0500

    Update video1.test.js

--- tests/video1.test.js
+++ tests/video1.test.js
@@ -9,4 +9,18 @@ test('My YouTube Test', async ({ page }) => {
   // Verify that the search input field is focused
   const isSearchInputFocused = await page.$eval('input#search', input => input === document.activeElement);
   expect(isSearchInputFocused).toBeTruthy();
+
+  // Type the search query in the search input field
+  await page.fill('input#search', 'I restored this 2$ ebay Junk Game Boy Color');
+
+  // Click on the search button
+  await page.click('button#search-icon-legacy');
+
+  // Wait for the search results to load
+  await page.waitForSelector('ytd-video-renderer');
+
+  // Click on the first video in the search results
+  await page.click('ytd-video-renderer');
+
+  await page.waitForTimeout(3000);
 });
